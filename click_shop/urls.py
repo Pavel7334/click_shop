58b13f3fff320f54730ec0360be359f4db791e94
Pavel7334 <novaya.rabota.pavla2016@yandex.ru>
2024-07-03 00:25:30 +0400

    Добавил оформление главной страницы подключил статику

--- click_shop/urls.py
+++ click_shop/urls.py
@@ -14,13 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from products.views import home_view
+
 urlpatterns = [
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    # path('products/', include('products.urls')),
+    path('products/', include('products.urls')),
     # path('orders/', include('orders.urls')),
     # path('payments/', include('payments.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
