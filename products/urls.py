438025eb2e9037257d67ad8b8e31f09cfa80afa7
Pavel7334 <novaya.rabota.pavla2016@yandex.ru>
2024-07-13 10:25:18 +0400

    Улучшил карточку товара по нажатию подробно, сделал для неё шаблон что бы подставлялось под каждый товар, расширил линейку продуктов

--- products/urls.py
+++ products/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -7,3 +9,7 @@ urlpatterns = [
     path('', home_view, name='home'),
     path('product/<str:slug>/', views.product_detail, name='product_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- products/views.py
+++ products/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, ProductImage
 
 
 def home_view(request):
     return render(request, 'products/home.html')
 
 
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/product_list.html', {'products': products})
+
+
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
-    return render(request, 'products/product_detail.html', {'product': product})
+    product_image = product.images.first()
+    context = {
+        'product': product,
+        'product_image': product_image,
+    }
+    return render(request, 'products/product_detail.html', context)
